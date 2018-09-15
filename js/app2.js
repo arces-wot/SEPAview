0b78c69a08e5855be9411f0c5839ddd8304d7c47
Shady96 <martina.verardi@gmail.com>
2018-09-15 13:01:07 +0200

    ora il form funziona

--- js/app2.js
+++ js/app2.js
@@ -2,7 +2,6 @@ let a = [];
 let y = [];
 a[0] = window.opener.x;
 y[0] = window.opener.tit;
-console.log(a.length);
 var max =[];
 var min = [];
 
@@ -15,8 +14,10 @@ var min = [];
 
 
 
+
+
 var host ="mml.arces.unibo.it";
-	
+
 var s = new ColorScheme;
 var colors = s.from_hue(10).scheme('analogic').variation("pastel").distance(0.5).colors();
 
@@ -61,16 +62,29 @@ function queryLiveData(callback) {
 	prefix += " prefix wgs84_pos:<http://www.w3.org/2003/01/geo/wgs84_pos#>";
 	prefix += " prefix time:<http://www.w3.org/2006/time#>";
 	prefix += " prefix sosa:<http://www.w3.org/ns/sosa/>";
-				
+
 	default_graph_uri = "http://wot.arces.unibo.it/monitor/mqtt/log";
+
+
+
+
+    var form ="<form id=\"calendar\">\n" +
+        "        date from: <input type=\"date\" name=\"dateFrom\"><br>\n" +
+        "        time from: <input type=\"time\" name=\"timeFrom\"><br>\n" +
+        "        date to: <input type=\"date\" name=\"dateTo\"><br>\n" +
+        "        time to: <input type=\"time\" name=\"timeTo\"><br>\n" +
+        "        </form>";
+
+    var buttonCambia = "<button id=\"cambia\">CAMBIA</button>";
+
+    $("#form").append(form);
+    $("#buttonCambia").append(buttonCambia);
+    
 
 
         utc = new Date();
         dateTo = new Date(utc.getTime()+2*3600*1000);
         dateFrom = new Date(dateTo.getTime()-24*3600*1000);
-
-        console.log(dateTo.toISOString());
-        console.log(dateFrom.toISOString());
 
         from = dateFrom.toISOString();
         from = from.substring(0,from.length-1);
@@ -82,6 +96,38 @@ function queryLiveData(callback) {
 
 
 
+
+    $('#buttonCambia').click(function() {
+
+        var timeAndDate = $('#calendar').serializeArray().reduce(function (obj, item) {
+            obj[item.name] = item.value;
+            return obj;
+        }, {});
+
+        console.log(timeAndDate.dateFrom + "T" + timeAndDate.timeFrom + ":00");
+
+            from = timeAndDate.dateFrom + "T" + timeAndDate.timeFrom + ":00";
+            to = timeAndDate.dateTo + "T" + timeAndDate.timeTo + ":00";
+
+
+        query = prefix
+            + " "
+            + "SELECT * WHERE {" + queries  + "FILTER(xsd:dateTime(?timestamp) > '" + from + "'^^xsd:dateTime && xsd:dateTime(?timestamp) < '" + to + "'^^xsd:dateTime)} ORDER BY ?timestamp"
+
+        url = "http://"+host+":8000/query?query=" + encodeURIComponent(query)
+            + "&default-graph-uri=" + encodeURIComponent(default_graph_uri);
+        xmlHttp.open("GET", url, true); // true for asynchronous
+
+        xmlHttp.setRequestHeader("Accept", "application/sparql-results+json");
+        xmlHttp.setRequestHeader("Content-Type", "application/sparql-query");
+
+        xmlHttp.send(null);
+
+    });
+
+
+
+
 //+ "UNION" + queries[1]
 
 
@@ -90,7 +136,7 @@ function queryLiveData(callback) {
 			+ "SELECT * WHERE {" + queries  + "FILTER(xsd:dateTime(?timestamp) > '" + from + "'^^xsd:dateTime && xsd:dateTime(?timestamp) < '" + to + "'^^xsd:dateTime)} ORDER BY ?timestamp"
 
 	url = "http://"+host+":8000/query?query=" + encodeURIComponent(query)
-			+ "&default-graph-uri=" + encodeURIComponent(default_graph_uri)
+			+ "&default-graph-uri=" + encodeURIComponent(default_graph_uri);
 	xmlHttp.open("GET", url, true); // true for asynchronous
 
 	xmlHttp.setRequestHeader("Accept", "application/sparql-results+json");
@@ -98,6 +144,9 @@ function queryLiveData(callback) {
 
 	xmlHttp.send(null);
 }
+
+
+
 
 var timer = window.setInterval(waitingTimer, 1000);
 var seconds = 0;
@@ -112,7 +161,7 @@ function results(json) {
 	var traces = [];
 	var layouts = [];
 
-	
+
 	window.clearInterval(timer);
 	jsapObj = JSON.parse(json);
 
@@ -154,7 +203,7 @@ function results(json) {
 		for (i in traces) {
             if (binding[traces[i].name] === undefined) continue;
 
-			
+
 			value = binding[traces[i].name].value;
 			traces[i].x.push(timestamp);
 			traces[i].y.push(value);
@@ -172,7 +221,7 @@ function results(json) {
 			if (i !== "0") {
 				index = parseInt(i, 10) + 1;
 				traces[i].yaxis = "y" + index;
-				
+
 				if (i === 1) {
 					layout.yaxis2.range[0] = min[i];
 					layout.yaxis2.range[1] = max[i];
@@ -196,7 +245,7 @@ function results(json) {
 	var element = document.createElement("div");
 	element.id = "all";
 	document.getElementById('plot').appendChild(element);
-	
+
 	Plotly.newPlot("all", data, layout);
 
 	// Trace by trace
