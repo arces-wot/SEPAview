f13c6da699c86ac88bbcda062a2641407dd107c0
Lorenzo Pisanò <lorenzopisa00@gmail.com>
2019-09-30 20:21:44 +0200

    Inserimento funzione redirectMap() in history.js e modifiche al metodo initMap() in map.js

---
+++ js/history.js
@@ -0,0 +1,375 @@
+let serverUTCOffset = 0;
+let lastHours = 24;
+
+let a = [];
+var max = [];
+var min = [];
+
+var s = new ColorScheme;
+
+// Using a fixed palette 
+var colors = ["#2980b9","#16a085"]
+
+
+var csvData;
+
+var observation = [];
+var title;
+var calendar;
+const selection = [];
+var traces = [];
+const axisLabels = ["y", "y2"]
+
+var place = null;
+var forecast = false;
+
+var layout = {
+	title : "No data",
+	mode: 'lines+markers',
+	xaxis : {
+		domain : [ 0.15, 1.0 ],
+		showgrid: true,
+	    zeroline: false,
+	    showline: false
+	},
+	titlefont : {
+		family : 'Verdana',
+		size : 22,
+		color : 'rgb(16,125,246)'
+	},
+	yaxis : {
+		range : [],
+		color : colors[0],
+		tickfont : {
+			family : 'Verdana',
+			size : 16,
+			color : colors[0]
+		},
+		title: 'yaxis',
+		showgrid: true,
+	    zeroline: false,
+	    showline: false
+	},
+	yaxis2: {
+		title: 'yaxis2',
+		titlefont: { color: 'rgb(148, 103, 189)' },
+		tickfont: {
+			family: 'Verdana',
+			size: 16,
+			color: colors[1]
+		},
+		overlaying: 'y',
+		side: 'right',
+		showgrid: true,
+		zeroline: false,
+		showline: false
+	},
+	margin:{
+		l : 0
+	}
+};
+
+function downloadCSV() {
+	let csvContent = "data:text/csv;charset=utf-8,";
+
+	csvData.forEach(function(rowArray) {
+	    let row = rowArray.join(",");
+	    csvContent += row + "\r\n";
+	});
+	
+	var encodedUri = encodeURI(csvContent);
+	window.open(encodedUri);
+}
+
+function onLoad() {
+	//Init plot
+	Plotly.newPlot("plot", [], layout, { responsive: true });
+
+	loadPlaceTree().then((tree) => {
+		$('#observations').treeview({
+			data: tree, 
+			levels: 1, //Define how many levels to expand
+			multiSelect : true, 
+			expandIcon: "fas fa-plus",
+			collapseIcon: "fas fa-minus",
+			onNodeSelected: function (event, data) {
+				serverTo = new Date(calendarTo.selectedDates[0].getTime());
+				
+				serverFrom = new Date(calendarFrom.selectedDates[0].getTime());
+			
+				selectObservation(data,serverFrom,serverTo)
+			},
+
+			onNodeUnselected: function (event,data) {
+				unselectObservation(data)
+				Plotly.newPlot("plot", traces, layout);
+			}
+		});
+		console.log();
+		
+		for (const obs of observation) {
+			const nodes =  $('#observations').treeview(true).findNodes(obs, "uri")
+			$('#observations').treeview(true).toggleNodeSelected(nodes)
+			$('#observations').treeview(true).revealNode(nodes)
+		}
+
+	})
+	
+	var parameters = window.location.search.substring(1).split("&");
+
+    var localTo = new Date();
+    var localFrom = new Date(localTo.getTime() - lastHours * 3600 * 1000);
+    
+	var i;
+	for (i=0; i < parameters.length; i++) {
+		switch(parameters[i].split("=")[0]) {
+		case "observation":
+			observation = decodeURIComponent(parameters[i].split("=")[1]).split(",");
+			break;
+		case "title":
+			title = unescape(unescape(parameters[i].split("=")[1]));
+			break;
+		case "forecast":
+			if (parameters[i].split("=")[1] == "true") forecast = true;
+			break;
+		case "place":
+			place = decodeURIComponent(parameters[i].split("=")[1]);
+			break;
+		}
+	}
+	
+	layout.title = title;
+	
+	// TODO: use updateFormUI
+	 if (forecast) $("#form").append("<input type='hidden' name='forecast' value=\"true\" />");
+	 if (place != null) $("#form").append("<input type='hidden' name='place' value=\""+place+"\" />");
+	 
+	calendarFrom = flatpickr("#from", {
+		mode : "single",
+		enableTime : true,
+		defaultDate: [localFrom],
+		time_24hr : true
+	});
+	
+	calendarTo = flatpickr("#to", {
+		mode : "single",
+		enableTime : true,
+		defaultDate: [localTo],
+		time_24hr : true
+	});
+}
+
+function selectObservation(node,from,to) {
+	if (selection.length > 1) {
+		const oldNode = selection.shift();
+		const t = $('#observations').treeview(true)
+		
+		// oldNode is just a clone of the real node cotained inside the tree.
+		// finding the node retrives the real one.
+		const treeNode = t.findNodes(oldNode.nodeId,"nodeId")
+		t.toggleNodeSelected(treeNode,{silent:true})
+	} 
+
+	selection.push(node)
+	
+	updateUIForm()
+
+	retriveObservedData(node.uri, from.toISOString(), to.toISOString())
+		.then((data) => {
+			data.name = node.text
+
+			updateTraces(data,node.symbol)
+			Plotly.newPlot("plot", traces, layout);
+		})
+}
+
+function unselectObservation(node) {
+	if(selection.length < 2){
+		traces.pop() 
+		selection.pop()
+		return
+	} 
+
+	if(node.nodeId === selection[0].nodeId){
+		traces.shift()
+		selection.shift()
+	}else{
+		traces.pop()
+		selection.pop()
+	}
+
+	updateUIForm()
+
+}
+
+function updateUIForm() {
+	let obs = ""
+	let title = ""
+	
+	for (const node of selection) {
+		obs += node.uri + ","
+		title += `${node.text}&`
+	}
+
+	obs = obs.substr(0, obs.length - 1)
+	title = title.substr(0,title.length -1)
+	layout.title = title
+
+	$("#form > input[name='observation']").attr("value", obs)
+	$("#form > input[name='title']").attr("value", escape(title))
+
+	//var parentId = selection[0].parentId;
+	//var original = parseInt(parentId, 10);
+
+	//alert(original);
+	//alert(parentId);
+}
+
+function redirectMap() {
+	//var currentNodeId = selection[0].nodeId;
+	var parentId = selection[0].parentId;
+
+	location.href = "../SEPAview-master/index.html";
+
+	var long = 10.717520;
+	var lat = 44.776585;
+	localStorage.setItem('lat', lat);
+	localStorage.setItem('long', long);
+	//map = L.map('mapid').setView([44.776585,10.717520], 15);
+	
+}
+
+function retriveObservedData(observation,from,to) {
+	// TIMESTAMPS are stored as local time using now() of SPARQL (e.g. "2019-06-26T12:32:47.0023")
+	if(forecast) return doForecastQuery(place, observation,from,to);
+	else return doQuery(observation,from,to);
+}
+
+function doForecastQuery(place,property,from,to) {
+	const sepa = Sepajs.client;
+
+	// PREFIXES
+	prefixes = "";
+	for (ns in jsap["namespaces"]) {
+		prefixes += " PREFIX " + ns + ":<" + jsap["namespaces"][ns]
+				+ ">";
+	}
+	
+	query = prefixes + " "
+	+ jsap["queries"]["DAILY_FORECAST"]["sparql"];
+
+	// Forced bindings
+	query = query.replace("?place", "<"+place+">");
+	query = query.replace("?property", "<"+property+">");
+	query = query.replace("?from", "'" + from.substr(0,10) + "'");
+	query = query.replace("?to", "'" + to.substr(0,10) + "'");
+
+	console.log("FORECAST Place: "+place + " Property: "+property);
+	console.log("From: "+from);
+	console.log("To: "+to);
+	 
+	return sepa.query(query,jsap).then((data)=>{ 
+		 return results(data);
+	 });	
+}
+
+function doQuery(observation,from,to) {
+	const sepa = Sepajs.client;
+
+	// PREFIXES
+	prefixes = "";
+	for (ns in jsap["namespaces"]) {
+		prefixes += " PREFIX " + ns + ":<" + jsap["namespaces"][ns]
+				+ ">";
+	}
+	
+	query = prefixes + " "
+	+ jsap["queries"]["LOG_QUANTITY"]["sparql"];
+
+	// Forced bindings
+	query = query.replace("?observation", "<"+observation+">");
+	query = query.replace("?from", "'" + from + "'^^xsd:dateTime");
+	query = query.replace("?to", "'" + to + "'^^xsd:dateTime");
+
+	console.log("Observation: "+observation);
+	console.log("From: "+from);
+	console.log("To: "+to);
+	 
+	return sepa.query(query,jsap).then((data)=>{ 
+		 return results(data);
+	 });	
+}
+
+function onRefresh() {
+
+	traces = []
+
+	serverTo = new Date(calendarTo.selectedDates[0].getTime());
+
+	serverFrom = new Date(calendarFrom.selectedDates[0].getTime());
+
+	retriveObservedData(selection[0].uri, serverFrom.toISOString(), serverTo.toISOString())
+		.then((data) => {
+			data.name = selection[0].text
+
+			updateTraces(data, selection[0].symbol)
+			Plotly.newPlot("plot", traces, layout);
+		}).then(() => {
+			retriveObservedData(selection[1] ? selection[1].uri : undefined, serverFrom.toISOString(), serverTo.toISOString())
+				.then((data) => {
+					data.name = selection[1].text
+
+					updateTraces(data, selection[1].symbol)
+					Plotly.newPlot("plot", traces, layout);
+				})
+		})	
+
+}
+
+function updateTraces(newTrace,unit) {
+	const layoutAxisKeys = ["yaxis","yaxis2"]
+	newTrace.unit = unit
+	
+	if (traces.length > 1) {
+		traces.shift()
+	}
+	traces.push(newTrace)
+
+	for (let i = 0; i < traces.length; i++) {
+		traces[i].line.color = colors[i]
+		traces[i].yaxis = axisLabels[i]
+		layout[layoutAxisKeys[i]].title = traces[i].unit
+	}
+}
+
+function results(jsapObj) {
+	
+	var trace = {
+		x : [],
+		y : [],
+		line : {
+			width : 1.25,
+		},
+		type : 'scatter',
+	};
+	
+	
+	csvData = [];
+
+	for (binding of jsapObj.results.bindings) {
+		timestamp = binding.timestamp.value;
+
+		// To local time
+		localTime = new Date(timestamp);
+		
+		value = parseFloat(binding.value.value);
+			
+		trace.x.push(localTime);
+		trace.y.push(value);
+			
+		// CSV
+		csvData.push([timestamp,value]);
+	}
+
+	return trace
+}
---
+++ js/map.js
@@ -0,0 +1,184 @@
+var placeIds = {};
+var markers = {};
+var map ;
+var icons = {};
+
+function initMap() {	
+	// Bologna
+	//map = L.map('mapid').setView([44.494048,11.343391], 13);
+	
+	// Bertacchini
+	var lat = localStorage.getItem('lat');
+	var long = localStorage.getItem('long');
+
+	if(lat != null || long != null)
+	{
+		map = L.map('mapid').setView([lat,long], 12);
+		//alert('mia');
+
+		localStorage.removeItem('lat');
+		localStorage.removeItem('long');
+	}else{
+		map = L.map('mapid').setView([44.776585,10.717520], 15);
+		//alert('sua');
+	}
+	
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'mapbox.streets',
+	    accessToken: 'pk.eyJ1IjoibHJvZmZpYSIsImEiOiJjanhiZjVxemkwYzZlM3pvODZjcGJlYjdtIn0.cD3reeNMoDGpiRBDTHn5_w'
+	}).addTo(map);
+	
+	// Init markers
+	initMarkers();
+	
+	// Draw layers
+	drawVectorLayers();
+	
+	// PREFIXES
+	prefixes = "";
+	for (ns in jsap["namespaces"]) {
+		prefixes += " PREFIX " + ns + ":<" + jsap["namespaces"][ns]
+				+ ">";
+	}
+	
+	query = prefixes + " "
+	+ jsap["queries"]["MAP_PLACES"]["sparql"];
+
+	const sepa = Sepajs.client;
+	
+	let places = sepa.subscribe(query,jsap);
+	places.on("added",addedResults=>{
+		added = addedResults["results"]["bindings"].length;
+		for (index = 0; index < added; index++) {
+            binding = addedResults.results.bindings[index];
+
+            place = binding.root.value;
+            name = binding.name.value;
+            lat = parseFloat(binding.lat.value.replace(",","."));
+            lng = parseFloat(binding.long.value.replace(",","."));
+
+            add_marker(lat,lng,name,place);
+        }
+		if (added > 0) refreshMap();
+	});
+
+	places.on("removed",removedResults=>{
+		removed = removedResults["results"]["bindings"].length;
+		for (index = 0; index < removed; index++) {
+            binding = removedResults.results.bindings[index];
+
+            place = binding.root.value;
+
+            remove_marker(place); 
+        }
+
+        if (removed > 0) refreshMap();	
+	});
+
+	//var temp = localStorage.getItem('coordinate'); coordinate per la mappa
+	//alert(temp);
+
+}
+
+
+
+function initMarkers() {
+	var leaf = L.icon({
+	    iconUrl: 'icon/Hoja.png',
+	    iconSize: [48, 48]
+	});
+	
+	icons["http://swamp-project.org/ns#Bertacchini"] = leaf;
+	icons["http://swamp-project.org/ns#Ferrari"] = leaf;
+	icons["http://swamp-project.org/ns#Bonacini"] = leaf;
+}
+
+function drawVectorLayers() {
+	var latlngs = [
+	    [[44.774522,10.723393],
+	     [44.779529, 10.723236],
+	     [44.785722, 10.715394],[44.788486,10.708209]]
+	];
+	
+	var polyline = L.polyline(latlngs, {color: 'blue'}).addTo(map);
+	// zoom the map to the polyline
+	map.fitBounds(polyline.getBounds());
+}
+
+
+
+function add_marker(lat, lng, name, id) {
+	if (placeIds[id] === undefined) placeIds[id] = generateID();
+	
+	var leaf = L.icon({
+	    iconUrl: 'icon/Hoja.png',
+	    iconSize: [24, 24]
+	});
+	
+	if (icons[id] != undefined) {
+		var marker = L.marker([lat, lng],{"title": name,"icon" : icons[id]}).addTo(map);
+		//marker.bindTooltip(name,{"permanent" : true});
+	}
+	else {
+		var marker = L.marker([lat, lng],{"title": name,}).addTo(map);
+		//marker.bindTooltip(name,{"permanent" : true});
+	}
+	
+	//TODO:adding marker clustering.
+	markers[id] = {};
+	markers[id]["name"] = name;
+	markers[id]["uri"] = id;
+	markers[id]["marker"] = marker;
+	
+	markers[id]["marker"].on('click', function() {
+		createObservationsNav(markers[id].uri,markers[id].name);
+	});
+
+	
+}
+
+function remove_marker(id) {
+	markers[id].marker.removeFrom(map);
+	
+	delete placeIds[markers[id].uri];
+	
+	delete markers[id];
+}
+
+function refreshMap() {
+//	positions = [];
+//	
+//	for (id in markers) {
+//		positions.push({lat : markers[id].marker.getPosition().lat(), lng : markers[id].marker.getPosition().lng()});	
+//	}
+//		
+//	if (positions.length === 0) {
+//		map.setCenter({lat: 44.494048, lng: 11.343391});
+//		map.setZoom(13);
+//	} else if (positions.length === 1) {
+//		map.setCenter(positions[0]);
+//	} else {	
+//		let neLat = positions[0].lat;
+//		let swLat = positions[0].lat;
+//
+//		let swLng = positions[0].lng;
+//		let neLng = positions[0].lng;
+//		
+//		for (index in positions) {			
+//			if (positions[index].lat > neLat) neLat = positions[index].lat;
+//			if (positions[index].lat < swLat) swLat = positions[index].lat;
+//			
+//			if (positions[index].lng > neLng) neLng = positions[index].lng;
+//			if (positions[index].lng < swLng) swLng = positions[index].lng;	
+//		}
+//		
+//		let sw = new google.maps.LatLng({lat: swLat, lng: swLng});
+//		let ne = new google.maps.LatLng({lat: neLat, lng: neLng});
+//		
+//		latlngbounds = new google.maps.LatLngBounds(sw,ne);
+//		
+//		map.fitBounds(latlngbounds);	
+//	}
+}
